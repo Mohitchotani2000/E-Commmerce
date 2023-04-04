170574f6362667d25b61c2eec9f55b0e5cdf8e58
Mohitchotani2000 <mohitchotani2000@gmail.com>
2023-04-04 10:04:55 +0530

    Modified

--- src/main/java/com/example/orderservice/OrderServiceAPIApplication.java
+++ src/main/java/com/example/orderservice/OrderServiceAPIApplication.java
@@ -3,6 +3,9 @@ package com.example.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author mohitchotani
+ */
 @SpringBootApplication
 public class OrderServiceAPIApplication {
 	public static void main(String[] args) {
