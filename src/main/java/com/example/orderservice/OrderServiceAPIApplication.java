9c668ee45c67dbf7e02b26f0a153de2894e98dad
Mohitchotani2000 <mohitchotani2000@gmail.com>
2023-04-04 10:01:25 +0530

    "Modified"

--- src/main/java/com/example/orderservice/OrderServiceAPIApplication.java
+++ src/main/java/com/example/orderservice/OrderServiceAPIApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class OrderServiceAPIApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(OrderServiceAPIApplication.class, args);
 	}
 }
