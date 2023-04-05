960d32bc7593e9b031e583455f043faed648eb2a
Mohitchotani2000 <mohitchotani2000@gmail.com>
2023-04-05 10:23:45 +0530

    updated

--- src/main/java/com/example/orderservice/OrderServiceAPIApplication.java
+++ src/main/java/com/example/orderservice/OrderServiceAPIApplication.java
@@ -1,5 +1,6 @@
 package com.example.orderservice;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author mohitchotani
  */
 @SpringBootApplication
+@Log4j2
 public class OrderServiceAPIApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceAPIApplication.class, args);
+//		log.info("Info message");
+//		log.error("Error message");
+//		log.warn("Warning message");
 	}
 }
